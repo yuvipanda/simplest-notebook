bf02e36ecc98e1564747fdcecd96459e9f6448c4
yuvipanda <yuvipanda@gmail.com>
2019-01-21 21:18:10 -0800

    Bump version

--- examples/notebook/setup.py
+++ examples/notebook/setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setuptools.setup(
     name="simplest-notebook",
-    version='0.0.4',
+    version='0.0.6',
     url="https://github.com/yuvipanda/simplest-notebook",
     author="Yuvi Panda",
     author_email="yuvipanda@gmail.com",
