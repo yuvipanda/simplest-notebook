76e0f3de4cb9f33dd47284b34858a0bc6b57faf2
yuvipanda <yuvipanda@gmail.com>
2019-04-28 15:07:35 -0700

    Build static assets before making package

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 import setuptools
+from distutils.command.build import build
+from distutils.command.sdist import sdist
+from setuptools.command.develop import develop
 from glob import glob
+import subprocess
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def build_static_first(cls):
+    """
+    Return a command that builds static assets before calling command Cls
+    """
+    class Command(cls):
+        def run(self):
+            command = [
+                "npm", "run", "build"
+            ]
+            subprocess.check_call(command, cwd=HERE)
+            super().run()
+
+    return Command
+
 
 setuptools.setup(
     name="simplest-notebook",
@@ -20,5 +42,10 @@ setuptools.setup(
     package_data={
         '': ['build/*', 'index.html']
     },
-    zip_safe=False
+    zip_safe=False,
+    cmdclass={
+        "sdist": build_static_first(sdist),
+        "build": build_static_first(build),
+        "develop": build_static_first(develop),
+    }
 )
